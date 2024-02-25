490e86291b31dcea3588f37ca4d2be0b01b8dd04
AngelAvirama <aviramadeoroangel@gmail.com>
2024-02-24 22:52:53 -0500

    Correcion algoritmo de euclides

--- Taller1/AlgoritmoEuclides.py
+++ Taller1/AlgoritmoEuclides.py
@@ -1,22 +1,11 @@
+#Implementa una función que encuentre el MCD de dos números utilizando el algoritmo de Euclides sin recursión.
+
 def algoritmo_euclides(a, b):
     if a > b:
         while b != 0:
             c = a
             a = b
             b = c % b
-        return a
-    else:
-        print("El numero a debe ser mayor al b")
-
-a = 48
-b = 18
-mcd = algoritmo_euclides(a, b)
-print(f"El MCD es {mcd}")
-
-def algoritmo_euclides(a, b):
-    if a > b:
-        while b != 0:
-            a, b = b, a % b
         return a
     else:
         print("El numero a debe ser mayor al b")
