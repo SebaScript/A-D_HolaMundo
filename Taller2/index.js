d1e6a122340356e5e54dba5a66f9d2cf2bae6cfa
SebaScript <juansgperez@gmail.com>
2024-03-16 16:43:38 -0500

    Primera implementación calculadoras

--- Taller2/index.js
+++ Taller2/index.js
@@ -1,1 +1,6 @@
-console.log("ola mundo!")
+function calcular() {
+    var m = document.getElementById('m_comb_rep').value;
+    var n = document.getElementById('n_comb_rep').value;
+    var resultado = Number(m) + Number(n); // Suma los valores de los inputs
+    document.getElementById('res_comb_rep').innerText = resultado; // Muestra el resultado en el span
+}
