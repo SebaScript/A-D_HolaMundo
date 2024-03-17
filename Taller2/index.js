725c1d8a9fb4feebc95c8cc7b899fdb3a90c2cd7
SebaScript <juansgperez@gmail.com>
2024-03-16 23:57:08 -0500

    calculadoras de principios agregadas

--- Taller2/index.js
+++ Taller2/index.js
@@ -1,11 +1,5 @@
 import {factorial} from "./factorial.js";
 import { permutacionRepetida } from "./permutacion.js";
-function calcular() {
-    var m = document.getElementById('m_comb_rep').value;
-    var n = document.getElementById('n_comb_rep').value;
-    var resultado = Number(m) + Number(n); // Suma los valores de los inputs
-    document.getElementById('res_comb_rep').innerText = resultado; // Muestra el resultado en el span
-}
 
 // Calculadora de permutación sin repetición
 document.getElementById('btn_perm_sin_rep').addEventListener('click', function PermutacionSinRep() {
