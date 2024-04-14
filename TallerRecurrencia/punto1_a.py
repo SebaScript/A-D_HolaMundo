1aae1621645618a5cc75432e05943afc56a6e641
SebaScript <juansgperez@gmail.com>
2024-04-14 01:42:35 -0500

    taller recurrencia inicial

---
+++ TallerRecurrencia/punto1_a.py
@@ -0,0 +1,49 @@
+from decimal import Decimal
+from math import ceil
+
+# Punto 1) a) recursivo
+
+def punto_1_a_recursivo(n):
+    if n == 0:
+        return 2
+    else:
+        return 5 * punto_1_a_recursivo(n-1)
+
+try:
+    # Ingresar n
+    n: int = 3
+    if n < 0:
+        print("n debe ser mayor o igual a cero")
+    else:
+        print("1) a) Recursivo: ", punto_1_a_recursivo(n)) # 250
+except TypeError:
+    print("Ingrese un n entero positivo mayor o igual que cero")
+except RecursionError:
+    print("Se ha ingresado un numero muy grande que supera la profundidad de recursión")
+
+# Punto 1) a) no recursivo:
+
+def punto_1_a_no_recursivo(n):
+    if n < 0:
+        return "n debe ser mayor o igual a cero"
+    
+    resultado = (Decimal(5) ** Decimal(n)) * Decimal(2)
+
+    # Se hace esta comprobación ya que el módulo Decimal a veces retorna valores incorrectos demasiado cercanos al número esperado
+    if (resultado - Decimal(0.00000001)) < resultado or (resultado + Decimal(0.00000001)) > resultado:
+        return ceil(resultado)
+    
+    return resultado
+
+""" En este caso si es posible ingresar n muy grandes, ya que la recursión no es un limitante en este caso y con el
+ módulo decimal se pueden obtener números muy grandes """
+
+try:
+    # Ingresar n
+    n: int = 3
+    if n < 0:
+        print("n debe ser mayor o igual a cero")
+    else:
+        print("1) a) No recursivo: ", punto_1_a_no_recursivo(n)) # 250
+except TypeError:
+    print("Ingrese un n entero positivo mayor o igual que cero")
---
+++ TallerRecurrencia/punto4_1.py
@@ -0,0 +1,53 @@
+from decimal import Decimal
+from math import ceil
+
+# N SIEMPRE DEBE SER MAYOR O IGUAL A 2
+
+# Punto 4) 1) recursivo
+
+def punto_4_1_recursivo(n):
+    if n == 0:
+        return 4
+    
+    if n == 1:
+        return 12
+    
+    else:
+        return (5 * (punto_4_1_recursivo(n-1))) + (6 * (punto_4_1_recursivo(n-2)))
+
+try:
+    # Ingresar n
+    n: int = 4
+    if n < 2:
+        print("n debe ser mayor o igual a dos")
+    else:
+        print("4) 1) Recursivo: ", punto_4_1_recursivo(n)) # 2964
+except TypeError:
+    print("Ingrese un n entero positivo mayor o igual que dos")
+except RecursionError:
+    print("Se ha ingresado un numero muy grande que supera la profundidad de recursión")
+
+# Punto 4) 1) no recursivo
+
+def punto_4_1_no_recursivo(n):
+    
+    resultado = (Decimal(12/7) * (Decimal(-1) ** Decimal(n)) + (Decimal(16/7) * Decimal(6) ** Decimal(n)))
+
+    # Se hace esta comprobación ya que el módulo Decimal a veces retorna valores incorrectos demasiado cercanos al número esperado
+    if (resultado - Decimal(0.00000001)) < resultado or (resultado + Decimal(0.00000001)) > resultado:
+        return ceil(resultado)
+    
+    return resultado
+
+
+try:
+    # Ingresar n
+    n: int = 4
+    if n < 2:
+        print("n debe ser mayor o igual a dos")
+    else:
+        print("4) 1) No recursivo: ", punto_4_1_no_recursivo(n)) # 2964
+except TypeError:
+    print("Ingrese un n entero positivo mayor o igual que dos")
+
+# FALTA AGREGAR PARA n = 151’145.018.
