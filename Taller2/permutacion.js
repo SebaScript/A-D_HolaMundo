a632a08adc0abe92068d0ab30b2442cd799c980d
SebaScript <juansgperez@gmail.com>
2024-06-10 21:51:53 -0500

    for -> for-of

--- Taller2/permutacion.js
+++ Taller2/permutacion.js
@@ -1,19 +1,27 @@
 import { factorial } from "./factorial.js";
 
-export function permutacionRepetida(n,stringNum){ //Complejidad en tiempo de la función: O(n**2);
+export function permutacionRepetida(n, stringNum) { 
+    // Complejidad en tiempo de la función: O(n**2)
     
-    const numeros = stringNum.split(",").map(numero => parseInt(numero)); //Complejidad en tiempo: O(1); Complejidad Espacial O(n)
+    const numeros = stringNum.split(",").map(numero => parseInt(numero)); 
+    // Complejidad en tiempo: O(1); Complejidad Espacial O(n)
 
-    let denominador = 1 //Complejidad en tiempo: O(1); Complejidad Espacial O(1)
+    let denominador = 1; 
+    // Complejidad en tiempo: O(1); Complejidad Espacial O(1)
     
-    for(let i = 0;i<numeros.length;i++){ //Complejidad en tiempo: O(n);
+    for (const numero of numeros) { 
+        // Complejidad en tiempo: O(n)
 
-        let num = factorial(numeros[i]) //Complejidad en tiempo: O(n**2); Complejidad Espacial O(1)
+        let num = factorial(numero); 
+        // Complejidad en tiempo: O(n**2); Complejidad Espacial O(1)
 
-        denominador*=num //Complejidad en tiempo: O(n); Complejidad Espacial O(1)
-        
+        denominador *= num; 
+        // Complejidad en tiempo: O(n); Complejidad Espacial O(1)
     }
-    return factorial(n)/denominador //Complejidad en tiempo: O(1);
+    
+    return factorial(n) / denominador; 
+    // Complejidad en tiempo: O(1)
 }
+
 //Ecuación: O(n**2) + 2 O(n) + 5 O(1)
 // O(n**2)
