3006874589340d136aa7cc4769aebfd8c48d8a3e
SebaScript <juansgperez@gmail.com>
2024-06-10 21:54:39 -0500

    for -> for-of

--- Taller2/principioAditivo.js
+++ Taller2/principioAditivo.js
@@ -1,15 +1,23 @@
-export function principioAditivo(numeros){ //Complejidad en tiempo de la función: O(n);
-    let listaNumeros = numeros.split(",").map(numero => parseInt(numero)); //Complejidad en tiempo: O(1); Complejidad Espacial O(n)
+export function principioAditivo(numeros) { 
+    // Complejidad en tiempo de la función: O(n)
 
-    let total = 0; //Complejidad en tiempo: O(1); Complejidad Espacial O(1)
+    let listaNumeros = numeros.split(",").map(numero => parseInt(numero)); 
+    // Complejidad en tiempo: O(1); Complejidad Espacial O(n)
 
-    for (let i = 0; i < listaNumeros.length; i++) { //Complejidad en tiempo: O(n)
+    let total = 0; 
+    // Complejidad en tiempo: O(1); Complejidad Espacial O(1)
 
-    total += listaNumeros[i]; //Complejidad en tiempo: O(n); Complejidad Espacial O(1)
-    
+    for (const numero of listaNumeros) { 
+        // Complejidad en tiempo: O(n)
+
+        total += numero; 
+        // Complejidad en tiempo: O(n); Complejidad Espacial O(1)
     }
-    return total; //Complejidad en tiempo: O(1)
+    
+    return total; 
+    // Complejidad en tiempo: O(1)
 }
+
 
 //Ecuacion: 2 O(n) + 5 O(1)
 // O(n)
