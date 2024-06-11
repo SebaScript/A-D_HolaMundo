0ae9c5e7d37e81b94fbed14ec810c69ab0df9325
SebaScript <juansgperez@gmail.com>
2024-06-10 21:30:09 -0500

    unnecesary if fixed

--- Taller2/index.js
+++ Taller2/index.js
@@ -5,6 +5,7 @@ import { principioMultiplicativo } from "./principioMultiplicativo.js";
 import {combinacionesConRepeticion, combinacionesSinRepeticion} from "./combinacion.js"
 import { variacionNoRepetida, variacionRepetida } from "./variacion.js";
 
+
 // Calculadora de permutación sin repetición
 document.getElementById('btn_perm_sin_rep').addEventListener('click', function PermutacionSinRep() {
 
@@ -19,6 +20,7 @@ document.getElementById('btn_perm_sin_rep').addEventListener('click', function P
         alert("Por favor, ingrese un valor válido que sea un número entero mayor o igual a 1 y tenga como máximo 3 cifras.")
     }
 });
+
 
 // Calculadora de permutación con repetición
 document.getElementById('btn_perm_con_rep').addEventListener('click', function PermutacionConRep() {
@@ -43,6 +45,7 @@ document.getElementById('btn_perm_con_rep').addEventListener('click', function P
     }
 });
 
+
 // Calculadora Principio aditivo
 document.getElementById("btn_prin_aditivo").addEventListener('click', function aditivo(){
     let numeros = document.getElementById("numeros_aditivo").value; //Complejidad en tiempo: O(1); Complejidad Espacial O(1)
@@ -53,7 +56,8 @@ document.getElementById("btn_prin_aditivo").addEventListener('click', function a
     } else {
         alert("Por favor, ingrese un valor válido que sea un número entero mayor o igual a 1 y tenga como máximo 3 cifras. Ingrese los números separados por 1 coma.")
     }
-})
+});
+
 
 //Calculadora Principio Multiplicativo
 document.getElementById("btn_prin_multiplicativo").addEventListener('click', function multiplicativo(){
@@ -65,7 +69,7 @@ document.getElementById("btn_prin_multiplicativo").addEventListener('click', fun
     } else {
         alert("Por favor, ingrese un valor válido que sea un número entero mayor o igual a 1 y tenga como máximo 3 cifras. Ingrese los números separados por 1 coma.")
     }
-})
+});
 
 
 //Calculadora Combinación con repetición
@@ -88,7 +92,7 @@ document.getElementById("btn_comb_rep").addEventListener('click', function combi
     } else {
         alert("Por favor, ingrese un valor válido que sea un número entero mayor o igual a 1 y tenga como máximo 3 cifras.")
     }
-})
+});
 
 
 //Calculadora Combinación sin repetición
@@ -112,7 +116,8 @@ document.getElementById("btn_comb_sin_rep").addEventListener('click', function c
     } else {
         alert("Por favor, ingrese un valor válido que sea un número entero mayor o igual a 1 y tenga como máximo 3 cifras.")
     }
-})
+});
+
 
 // Calculadora variación con repetición
 document.getElementById("btn_var_rep").addEventListener('click', function variacionRep(){
@@ -134,7 +139,7 @@ document.getElementById("btn_var_rep").addEventListener('click', function variac
     } else {
         alert("Por favor, ingrese un valor válido que sea un número entero mayor o igual a 1 y tenga como máximo 3 cifras.")
     }
-})
+});
 
 
 
@@ -158,15 +163,13 @@ document.getElementById("btn_var_sin_rep").addEventListener('click', function va
     } else {
         alert("Por favor, ingrese un valor válido que sea un número entero mayor o igual a 1 y tenga como máximo 3 cifras.")
     }
-})
+});
 
 
 function validarInput(valor) {
-    if (valor.length > 3 || isNaN(valor) || !Number.isInteger(parseFloat(valor)) || parseFloat(valor) < 1) {
-        return false
-    }
-    return true
-}   
+    return !(valor.length > 3 || isNaN(valor) || !Number.isInteger(parseFloat(valor)) || parseFloat(valor) < 1)
+};
+
 
 function validarListaNumeros(lista) {
     const numeros = lista.split(',')
@@ -179,7 +182,8 @@ function validarListaNumeros(lista) {
     }
 
     return true;
-}
+};
+
 
 function validarRepeticionPermutacion(n, lista) {
     const numeros = lista.split(',')
@@ -194,4 +198,4 @@ function validarRepeticionPermutacion(n, lista) {
     }
 
     return true;
-}
+};
