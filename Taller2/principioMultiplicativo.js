29875b139471733755ee7dddec36f5779b12b8cd
SebaScript <juansgperez@gmail.com>
2024-06-10 21:56:13 -0500

    for -> for-of

--- Taller2/principioMultiplicativo.js
+++ Taller2/principioMultiplicativo.js
@@ -1,14 +1,22 @@
-export function principioMultiplicativo(numerosMultiplicativo){ //Complejidad en tiempo de la función: O(n);
-    let listaNumeros = numerosMultiplicativo.split(",").map(numero => parseInt(numero)); //Complejidad en tiempo: O(1); Complejidad Espacial O(n)
+export function principioMultiplicativo(numerosMultiplicativo) { 
+    // Complejidad en tiempo de la función: O(n)
 
-    let total = 1; //Complejidad en tiempo: O(1); Complejidad Espacial O(1)
+    let listaNumeros = numerosMultiplicativo.split(",").map(numero => parseInt(numero)); 
+    // Complejidad en tiempo: O(1); Complejidad Espacial O(n)
 
-    for (let i = 0; i < listaNumeros.length; i++) { //Complejidad en tiempo: O(n)
+    let total = 1; 
+    // Complejidad en tiempo: O(1); Complejidad Espacial O(1)
 
-    total *= listaNumeros[i]; //Complejidad en tiempo: O(n) ; Complejidad Espacial O(1)
-    
+    for (const numero of listaNumeros) { 
+        // Complejidad en tiempo: O(n)
+
+        total *= numero; 
+        // Complejidad en tiempo: O(n); Complejidad Espacial O(1)
     }
-    return total; //Complejidad en tiempo: O(1)
+    
+    return total; 
+    // Complejidad en tiempo: O(1)
 }
+
 //Ecuacion: 2 O(n) + 5 O(1)
 // O(n)
